b570da295a3419ddda1a53df51f78b97d7e1c697
Robel Alemayehu Gebreyohannes <robelalemayehu838@gmail.com>
2025-10-19 20:41:07 +0300

    Fix case sensitivity in Customers table name

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
-INSERT INTO Customers(customer_id, customer_name, email, adress) 
+INSERT INTO customers(customer_id, customer_name, email, adress) 
 VALUES (19, 'Robel Alemayehu', 'robaalex19@gmail.com', 'Addis Ababa');
