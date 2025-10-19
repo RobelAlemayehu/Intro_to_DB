43447ba9bcb84a62c4bc82cf3fd1c95929d1b1f6
Robel Alemayehu Gebreyohannes <robelalemayehu838@gmail.com>
2025-10-19 20:42:22 +0300

    Update customer record in task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
 INSERT INTO customers(customer_id, customer_name, email, adress) 
-VALUES (19, 'Robel Alemayehu', 'robaalex19@gmail.com', 'Addis Ababa');
+VALUES (1, 'Cole Baidoo, 'cbaido@sandtech.com', '123 Happiness Ave');
