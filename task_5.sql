25f7e3f7214dce270dec8d1e880a98baf016c135
Robel Alemayehu Gebreyohannes <robelalemayehu838@gmail.com>
2025-10-19 20:42:55 +0300

    Fix typo in customers table insert statement
    Corrected a typo in the 'address' field.

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
-INSERT INTO customers(customer_id, customer_name, email, adress) 
+INSERT INTO customers(customer_id, customer_name, email, address) 
 VALUES (1, 'Cole Baidoo, 'cbaido@sandtech.com', '123 Happiness Ave');
